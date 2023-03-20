d6f33c79011d2c749a53739b4cab715484c4a18d
rishi000000007 <111013961+rishi000000007@users.noreply.github.com>
2023-03-21 00:38:33 +0530

    This is new feature2

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -8,7 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getdata()
         Toast.makeText(this,"happpy",Toast.LENGTH_SHORT).show()
 
     }
+    private fun getdata()
+    {
+        Toast.makeText(this,"happpy",Toast.LENGTH_SHORT).show()
+    }
+
 }
